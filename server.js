6aa07716d933bb4ecda06681d328bded28d347df
sathyaprakash195 <sathyaprakash195@gmail.com>
2020-06-01 17:32:28 +0530

    commit

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var dbconnection=require('./conn');
 var mongoose=require('mongoose');
 var bodyParser=require('body-parser')
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 const path=require('path');
 var productsroute=require('./routes/products')
@@ -16,6 +15,7 @@ app.use('/api/products/',productsroute);
 app.use('/api/user/',userroute);
 app.use('/api/order/',orderroute);
 app.use('/api/admin/',adminroute);
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT=process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
